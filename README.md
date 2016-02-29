0fcf39a612f921953cd48153fd3c2a71318ca909
akata in mac book pro <akata.onen@gmail.com>
2016-02-29 17:03:41 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# objective-c-single-view-application-sample-02
+
+---
+
+[http://dev.classmethod.jp/smartphone/iphone/introduction-to-storyboarding-2/](http://dev.classmethod.jp/smartphone/iphone/introduction-to-storyboarding-2/)
---
+++ single-view-application-02/ViewController.m
@@ -0,0 +1,21 @@
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+- (IBAction)mainViewReturnActionForSegue:(UIStoryboardSegue *)segue {
+}
+
+@end
